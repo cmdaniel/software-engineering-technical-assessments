6d3e56e8e073287dfa7965310c1a21f6c73283a5
developer <developer@industria-i.com.br>
2026-03-15 13:05:33 +0000

    Refactor code structure and enhance type hints for clarity
    - Updated type hints in main.py, results_controller.py, and results_service.py for better type safety.
    - Improved the get_result method to return a more informative response.
    - Added unittest configuration in settings.json for better test management.
    - Introduced model classes for structured data representation.

---
+++ election-api-python/src/model/model.py
@@ -0,0 +1,54 @@
+from collections import defaultdict
+from dataclasses import dataclass, field
+from decimal import Decimal
+from enum import Enum
+
+
+class PartyEnum(Enum):
+    LAB = "LAB"
+    CON = "CON"
+    LD = "LD"
+    PC = "PC"
+    OTH = "OTH"
+    GRN = "GRN"
+    UKIP = "UKIP"
+    NOONE = "noone"
+    WINNER = "winner"
+
+
+class PartyResultEnum(Enum):
+    VOTES = "votes"
+    SHARES = "shares"
+
+
+@dataclass
+class Scoreboard:
+    party_seats: dict[PartyEnum, int]
+    overall_winner: PartyEnum
+    party_result: dict[PartyEnum, dict[str, dict[PartyEnum, int | Decimal]]]
+    is_tied: bool = False
+
+
+@dataclass
+class PartyResult:
+    party: PartyEnum
+    votes: int
+    share: Decimal
+
+
+@dataclass
+class Constituency:
+    id: int
+    name: str
+    seqNo: int
+    PartyResults: list[PartyResult]
+
+
+@dataclass
+class FlatConstituency:
+    id: int
+    name: str
+    seqNo: int
+    party: PartyEnum
+    votes: int
+    share: Decimal
